2547399de0529647a6610be7eee490e63894368c
Krissy <35669945+kmschwabs3@users.noreply.github.com>
2018-12-18 20:17:17 -0600

    created function to create pet from input values

--- js/digipet.js
+++ js/digipet.js
@@ -1,1 +1,25 @@
-// alert('hello!');
+function createPet(){
+
+	var animalName = document.getElementById('petName').value;
+	var typeAnimal = document.getElementsByName('gridRadios');
+	var type = '';
+
+		// loops through radio buttons
+	for (var i = 0; i < typeAnimal.length; i++) {
+		if(typeAnimal[i].checked){
+			type = typeAnimal[i].value;
+			break;
+		}
+	}
+	// create object
+	var digiPet = {
+		digiPetName: animalName,
+		digiPetType: type,
+		health: 100,
+		feeding: 100,
+		pets: 100,
+		sleep:100,
+		playTime: 100
+	}
+	alert(digiPet);
+}
